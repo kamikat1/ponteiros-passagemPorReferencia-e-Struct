0342d4b467f16ee4ed69dd6ea8769e690bbfcab7
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-06-04 22:29:17 -0300

    anotações sobre o funcionamento de ponteiros

---
+++ um.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *pti;
+    int i = 10;
+    pti = &i;
+    printf("%d\n", pti); //imprime o conteudo da variavel pt1, que esta guardando o endereço de i, 6422036
+    printf("%d\n", *pti); //imprime para onde o ponteiro pti esta apontando, no caso para a variavel i, que esta guardando 10, então imprime 10
+    printf("%d\n", &i); //endereço de memoria de onde esta a variavel i: 6422036
+    printf("%d\n", &pti); //endereço de memoria da variavel pti 
+    printf("%d\n", &*pti); //endereço de memoria de onde *pti está apontando 
+
+}
