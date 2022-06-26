aab3019f03a26ac16b754cf62facac04998d53f0
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-06-26 16:28:18 -0300

    Create structPessoa.c

---
+++ structPessoa.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pessoa {
+    char nome[10];
+    int idade;
+    float altura;
+    float peso;
+} person;
+
+int main(){
+   person data[100];
+
+   printf("Digite os dados de 100 pessoas: \n");
+
+   for(int i=0; i<2; i++){
+        printf("Nome: ");
+        scanf("%s", &data[i].nome);
+        printf("Idade: ");
+        scanf("%d", &data[i].idade);
+        printf("Altura: ");
+        scanf("%f", &data[i].altura);
+        printf("Peso: ");
+        scanf("%f", &data[i].peso);
+   }
+
+
+    for(int i=0; i<2; i++){
+        if( data[i].idade < 18 && data[i].peso > 70){
+            printf("%s\n", data[i].nome);
+        }
+    }
+
+}
